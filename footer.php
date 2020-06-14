71e02ab5a8be797c513ce9fe903bbf5d760bf412
OrangeFoxie <vinhphuc931998@gmail.com>
2020-06-14 22:37:48 +0700

    update list product 1

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div>
 <footer style="position: absolute; margin-top:0;botom:0;left:50%; transform:translateX(-50%);-ms-user-select:none;-webkit-user-select: none;">
-    &#169; Footer here
+    Footer here
 </footer>
 </body>
 
